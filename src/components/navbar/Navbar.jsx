b39307decb549d8bf7441786a26b57c19e4c5cfe
sshak <shawnshaknitz@gmail.com>
2024-01-31 21:03:22 -0500

    maybe get rid of home nav and click on name?

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <Image src="/Logo.png" alt="" width={60} height={60} />
-      <div className={styles.slogan}>Carena Nottoli</div>
+      <Link href="/" className={styles.slogan}>
+        <h1>Carena Nottoli</h1>
+      </Link>
       <div>
         <Links />
       </div>
