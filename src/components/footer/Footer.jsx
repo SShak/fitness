862e9f313922381418538c9940b8147ed538b02e
sshak <shawnshaknitz@gmail.com>
2024-02-01 20:23:30 -0500

    looking better

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,11 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.passage}>
-        Phillippians 4:13 - I can do all things through Christ who gives me
-        strength.
+        <h1>
+          {" "}
+          Phillippians 4:13 - I can do all things through Christ who gives me
+          strength.
+        </h1>
       </div>
     </div>
   );
