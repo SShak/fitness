8ea53392e8b180e1e98c253d2e1eab1af76cddfd
sshak <shawnshaknitz@gmail.com>
2024-02-11 13:25:54 -0500

    contact form start

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/logoFull.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="What brings you to my page?"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
