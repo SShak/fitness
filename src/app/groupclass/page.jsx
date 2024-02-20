44d110dea03669bb6f1f1532db372f9437dcf5b2
sshak <shawnshaknitz@gmail.com>
2024-02-19 20:10:35 -0500

    fixing group classes

--- src/app/groupclass/page.jsx
+++ src/app/groupclass/page.jsx
@@ -5,24 +5,9 @@ const GroupClass = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1 className={styles.slogan}>
-          Adult Fitness- Overall Health and Wellness Training
-        </h1>
+        <h1 className={styles.slogan}>Group Classes</h1>
         <p className={styles.desc}>
-          1hr HIIT Training: High intensity total body training including a
-          combination of strength, cardio, and core exercises. Why is HIIT
-          training beneficial? This intense form of exercise strives to push
-          your muscles to the maximum. Meaning, this is a great form of training
-          to reduce body fat, increase your heart rate, and improve both
-          strength and cardiovascular fitness{" "}
-        </p>
-        <p className={styles.desc}>
-          1hr Strength Training: Total body strength training utilizing
-          dumbbells, barbells, medicine balls, resistance bands, etc. Why is
-          Strength training beneficial? Yes, strength training makes you
-          stronger. As adults, it is important that we work on our muscles and
-          joints. Not only that, but strength training builds bone density,
-          assists weight loss, builds muscle mass, and muscle tone.{" "}
+          For interest in group classes blah blah blah contact me below{" "}
         </p>
       </div>
       <div className={styles.imageContainer}>
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -53,6 +53,25 @@ const ServicesPage = () => {
         barbells- Standard and Hexbars. 2) Dumbbell exercises. 3)
         Plyometric/Power exercises- Example: Box Jumps. 4) Core work. 5) Daily
         tracking of weights- Keep track of progress!
+        <h1 className={styles.slogan}>
+          Adult Fitness- Overall Health and Wellness Training
+        </h1>
+        <p className={styles.desc}>
+          1hr HIIT Training: High intensity total body training including a
+          combination of strength, cardio, and core exercises. Why is HIIT
+          training beneficial? This intense form of exercise strives to push
+          your muscles to the maximum. Meaning, this is a great form of training
+          to reduce body fat, increase your heart rate, and improve both
+          strength and cardiovascular fitness{" "}
+        </p>
+        <p className={styles.desc}>
+          1hr Strength Training: Total body strength training utilizing
+          dumbbells, barbells, medicine balls, resistance bands, etc. Why is
+          Strength training beneficial? Yes, strength training makes you
+          stronger. As adults, it is important that we work on our muscles and
+          joints. Not only that, but strength training builds bone density,
+          assists weight loss, builds muscle mass, and muscle tone.{" "}
+        </p>
       </div>
     </div>
   );
